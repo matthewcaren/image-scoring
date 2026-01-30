6d96ab2d32b975d7b3cc18311487ee0b1d0bbe81
Matthew Caren <matt.t.caren@gmail.com>
2026-01-29 16:47:38 -0800

    add audio check, mongo logging to matching study

---
+++ experiments/choice-task/js/game_settings.js
@@ -0,0 +1,20 @@
+gs = {
+    study_metadata: {
+        project: "image-scoring", // mongo dbname
+        experiment: "image-scoring-matching", // mongo colname
+        version: '1.0.0',
+        study_description: 'matching sounds to video clips',
+        date: new Date().toISOString().split('T')[0],
+        time: new Date().toISOString().split('T')[1].split('.')[0],
+    },
+    session_info: {
+        condition: undefined,
+        stimulus: undefined,
+        send_data: undefined
+    },
+    prolific_info: {
+        prolificPID: undefined,
+        prolificStudyID: undefined,
+        prolificSessionID: undefined
+    }
+}
